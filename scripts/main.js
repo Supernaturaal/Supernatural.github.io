138f67852bdfd834188701e4170ba8841b86bffb
Supernaturaal <61050453+Supernaturaal@users.noreply.github.com>
2020-04-17 13:28:32 +0300

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,7 +1,4 @@
-﻿var myHeading = document.querySelector('h1');
-myHeading.textContent = 'Зимние виды спорта';
-var myVariable = document.querySelector('h1');
-alert('Дороу!');
+
 
 window.onload = function(){
  window.setInterval(function(){
