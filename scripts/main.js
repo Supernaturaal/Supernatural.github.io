f9f597b1804cb237419dce10ecea7f83cfafbc77
Supernaturaal <61050453+Supernaturaal@users.noreply.github.com>
2020-04-17 13:28:46 +0300

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,12 +1,6 @@
 
 
-window.onload = function(){
- window.setInterval(function(){
-      var now = new Date();
-       var clock = document.getElementById("clock");
-     clock.innerHTML = now.toLocaleTimeString();
- }, 100);
-};
+
 const para = document.querySelector('p');
 
 para.addEventListener('click', updateName);
