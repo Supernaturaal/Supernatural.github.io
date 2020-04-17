826885b5dfaecea8c33b6cd9f1ee2ed2a95ea2c3
Supernaturaal <61050453+Supernaturaal@users.noreply.github.com>
2020-04-17 13:16:06 +0300

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -11,13 +11,4 @@ window.onload = function(){
  }, 100);
 };
 
-var value = true //условие появления кнопки, true - появиться, false - нет 
-var btn = document.createElement('button');//создаём нашу кнопку
 
-var textInBtn = document.createTextNode('BUTTON');//создаем текст для кнопки
-
-btn.appendChild(textInBtn);//добавляем текст в кнопку
-
-if(value){//в зависимости от условия добавляем кнопку в документ
-  document.body.appendChild(btn);
-} 
