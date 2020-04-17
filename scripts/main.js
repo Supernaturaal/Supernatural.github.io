58425a4d259a9c4728fc80abfbb50c755a94411e
Supernaturaal <61050453+Supernaturaal@users.noreply.github.com>
2020-04-17 13:19:18 +0300

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -10,5 +10,13 @@ window.onload = function(){
      clock.innerHTML = now.toLocaleTimeString();
  }, 100);
 };
+const para = document.querySelector('p');
+
+para.addEventListener('click', updateName);
+
+function updateName() {
+  let name = prompt('Enter a new name');
+  para.textContent = 'Нажми на меня ' + name;
+}
 
 
