41205cc9ffcc4a6b780b1f363aba7f0fce02a564
Supernaturaal <61050453+Supernaturaal@users.noreply.github.com>
2020-06-19 13:59:57 +0300

    Add files via upload

--- scripts/main.js
+++ scripts/main.js
@@ -9,3 +9,20 @@ const buttons = document.querySelectorAll('button');
 for(let i = 0; i < buttons.length ; i++) {
   buttons[i].addEventListener('click', createParagraph);
 }
+
+var myButton = document.querySelector('button');
+var myHeading = document.querySelector('h1');
+function setUserName() {
+  var myName = prompt('Введи свое имя.');
+  localStorage.setItem('name', myName);
+  myHeading.textContent = 'Привет, ' + myName;
+}
+if(!localStorage.getItem('name')) {
+  setUserName();
+} else {
+  var storedName = localStorage.getItem('name');
+
+}
+myButton.onclick = function() {
+  setUserName();
+}
