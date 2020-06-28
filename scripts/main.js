88458e341cd6c9da00aa0bfb2e5be09bc164ea18
Supernaturaal <61050453+Supernaturaal@users.noreply.github.com>
2020-06-28 10:14:17 +0300

    Add files via upload

--- scripts/main.js
+++ scripts/main.js
@@ -1,14 +1,3 @@
-function createParagraph() {
-  let para = document.createElement('p');
-  para.textContent = 'Перестань.';
-  document.body.appendChild(para);
-}
-
-const buttons = document.querySelectorAll('button');
-
-for(let i = 0; i < buttons.length ; i++) {
-  buttons[i].addEventListener('click', createParagraph);
-}
 
 var myButton = document.querySelector('button');
 var myHeading = document.querySelector('h1');
